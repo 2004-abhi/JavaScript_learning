0f1e5ccb6f0abf6567f2452b4558f4ece0861d81
2004-abhi <abhinandharilalv@gmail.com>
2026-09-10 12:34:35 +0530

    loop

---
+++ day9/loop.js
@@ -0,0 +1,55 @@
+let i=1;
+for (; i<=5;i++){
+    console.log(i);
+    
+}
+
+let a=1;
+for(;true;a++){
+    console.log(a);
+    
+}
+
+// for (let b=1;b<=5;){
+//     console.log(b);
+//      b++;   
+// }
+
+// for(let c=1;c<=5;){
+//     c++;
+//     console.log(c);
+    
+// }
+
+// //while loop
+// let actualPassword="1315 ";
+// let userPassword=prompt("enter your password ")
+// while(userPassword!==actualPassword){
+//     userPassword=prompt("enter your password ")
+// }
+// console.log("enter is successful ");
+
+
+//do while loop
+// let i=1;
+// do{
+//     console.log("it will execute once process of correct condition ");
+//     i++
+// }
+// while(i<=5)
+
+
+for(let i=1;i<=5;i++)
+{
+    if(i%2==0){
+        console.log(i);
+        
+    }
+}
+
+for(let z=1;z<=5;z++){
+    if(z%2!==0){
+        console.log(z);
+        
+    }
+}
