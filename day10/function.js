138b80bf214bf77887510ad49328bf9f0a3ee920
2004-abhi <abhinandharilalv@gmail.com>
2026-09-11 12:17:12 +0530

    function

---
+++ day10/function.js
@@ -0,0 +1,35 @@
+// function greeting(){
+// console.log("good morning ");
+// console.log("go to mental clinic ......");
+// }
+//  greeting()
+
+function coffeeMachine(customer,coffeeType,price ){
+    console.log("Type of coffee:",coffeeType);
+    console.log(hotelTaj);
+    console.log("boil water");
+     console.log("add milk"); 
+     console.log("add coffee powder");
+      console.log("add sugar"); 
+     console.log("pour coffee of cup");
+    console.log("total price:",price);
+     console.log("prepared coffee for:",customer);
+   
+}
+  coffeeMachine("rose","cold coffee",65);
+
+
+
+
+
+function Message(company)
+{
+    for(let i = 1 ; i <= 50 ; i++){
+        console.log(i);
+        console.log("join the great company : ",company);
+        console.log("Feel the actual Heaven");
+
+    }
+}
+
+Message("TestYantra")
