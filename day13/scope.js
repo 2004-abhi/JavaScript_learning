0f6bd50fb5f37608fbea2223f981b215fad92e21
2004-abhi <abhinandharilalv@gmail.com>
2026-09-17 12:08:46 +0530

    scopes

---
+++ day13/scope.js
@@ -0,0 +1,60 @@
+// var a=10;
+// let b=30;
+// const c=30;
+
+// console.log("ouside , ",a);
+// console.log("outside , ",b);
+// console.log("outside , ",c);
+
+
+
+// function f1(){
+//     console.log("inside function ",a);
+//     console.log("inside function ",b);
+//     console.log("inside function ",b);
+// }
+// f1()
+
+
+// if (true){
+//       console.log("inside function ",a);
+//     console.log("inside function ",a);
+//     console.log("inside function ",a);
+// }
+
+// console.log("ouside , ",a);
+// console.log("outside , ",b);
+// console.log("outside , ",c);
+
+
+//function scope
+// function abu(){
+//     var user1= "rose ";
+//     let user2="abyyy "
+//     const user3="arya "
+
+//     console.log("nee1 ",user1);
+//     console.log("nee2 ",user2);
+//     console.log("nee2 ",user3);
+    
+// }
+// abu()
+//  console.log("nee1 ",user1);
+//     console.log("nee2 ",user2);
+//     console.log("nee2 ",user3);
+
+
+    // block scope
+if("qspiders "){
+    var isBatteriesAvailable=false;
+    let studentCount=700;
+    const brand="banloree ";
+
+    console.log("is batterties are avilable: ",isBatteriesAvailable);
+    console.log("total no of nee:",studentCount);
+    console.log("brand name :",brand);
+
+}   
+  console.log("is batterties are avilable: ",isBatteriesAvailable); 
+    console.log("brand name :",brand);
+    console.log("total no of nee:",studentCount);
