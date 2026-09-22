c4bc4e5b83d8c3df5d6ce3ec719b4055eb891208
2004-abhi <abhinandharilalv@gmail.com>
2026-09-22 12:20:23 +0530

    postional

---
+++ day14/strin.js
@@ -0,0 +1,113 @@
+// strings-sequence of character used to represent text data.
+// using single quote
+// let city = 'Kochi';
+// console.log("using single quote",city);
+
+// //using double quotes
+// let state = "Kerala";
+// console.log("Using double quote",state);
+
+// //using backtick (template literal)
+// let info=`we have e days
+// holiday`
+// console.log(info);
+
+
+//HOw to access String Characters
+
+// let name="Arjun"; //012345
+// console.log(name);
+// console.log(name[0]); //accessing
+// console.log(name[5]); //accessing
+
+// name[0]="B "
+// console.log(name);
+// name="krujun ";
+// console.log(name);
+// console.log(name[-1]);
+// console.log(name[6]);
+
+
+// //string - interpolation -> insertin variables or expressions inside strin
+// let namee = "Karthik";
+// let age = 20;
+// console.log("My name is",namee,"; I am ",age," years old!!");
+// console.log(My name is ${name} and i am ${age} years old....);
+
+
+// let a = 10
+// let b = 20
+// console.log(sum of ${a} + ${b} = ${a + b});
+
+
+// let password = "Secret";
+// console.log(password.length);
+
+// if(password > 8){
+//     console.log("Valid")
+// }
+// else{
+//     alert("Invalid pasasword")
+// }
+
+
+// let user = "Kalam"
+
+// user.toUpperCase(user)
+// console.log(user);
+
+// let result = user.toUpperCase();
+
+// console.log(result);
+
+
+
+// let user="abyyy "
+// user.toUpperCase()// returin new strin but we are not storin inside variable
+// console.log(user);
+
+
+// let user="abyyy "
+// let result=user.toUpperCase()//storin in a new variable
+// console.log(result);
+// console.log(user);
+
+
+// let user="kalam ";
+// console.log(user);
+// user=user.toUpperCase//storin in existin variables
+// console.log(user);
+
+
+
+// let movie="PVR CINEAMS ";
+// movie=movie.toLowerCase()
+// console.log(movie);
+
+
+
+// //trim
+// let student="   AMMU   ";
+// console.log(student.length);
+// student=student.trim()
+// console.log(student.length);
+
+
+//E-commerce product details
+//used displayin product information suc as product name and price
+let product="laptop ";
+let price="55000 ";
+
+let message =`product: ${product},price:${price}`;
+
+console.log(message);
+//product: laptop,price:55000
+
+
+
+// user profile /  welcome messae
+// used after login to a personalized  welcome message.
+
+
+
+
