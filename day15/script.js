61c11bef54a437219b425b5618f2fa13fbb0185f
2004-abhi <abhinandharilalv@gmail.com>
2026-09-23 12:12:50 +0530

    function

---
+++ day15/script.js
@@ -0,0 +1,70 @@
+// let user="kalam "//01234
+// let result=user.charAt(-4)
+// console.log(user);
+// console.log(result);
+
+// let user="kalam "//01234
+// let a=user.indexOf("l")
+// console.log(a);
+
+
+// let user="kalamVenkit "//0123454678910
+// let a=user.charCodeAt(6)  //its asci code
+// console.log(a);
+
+
+// let message="I like icecream. icecream is tasty...😁 ";
+// message = message.replace("icecream","vannila icecream ")
+// // message=message.replaceAll("icecream","vannila icecream ")
+// console.log(message);
+
+
+
+//endsWith()
+// let company = "Wipro";
+// let res = company.startsWith("W");
+// let res2 = company.endsWith("o");
+// let res3 = company.endsWith("O")
+// console.log(res);
+// console.log(res2);
+// console.log(res3);
+
+
+// let contact="9879";
+// let notification=contact.padStart(10,"X")
+// console.log("OTP sent to your mobile number ends :",notification);
+
+// let contact="9879";
+// let notification=contact.padEnd(10,"X")
+// console.log("OTP sent to your mobile number ends :",notification);
+
+
+//concat()- it is used to join two or more strin and return a new "new strin ".
+//it does not erase oriinal strin
+//it returns a new combined strin
+// works same as + operator only
+// now a days we are usin + operator only
+
+
+// let first="aelloo";
+// let second="world";
+// let newString=first.concat(" ",second)
+// console.log(newString);//usin concat metod
+// console.log(first+""+second);//usin + operator
+// console.log(newString);
+// console.log(first);
+// console.log(second);
+
+
+// length
+
+function checkPassword(password){
+    if (password.length >= 8){
+        return "Password is strong";
+    }else {
+        return "Password must contain at least 8 characters";
+    }
+} 
+console.log(checkPassword("Java@1234"));
+
+
